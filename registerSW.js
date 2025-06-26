05825ed158ecee7dc29d66b4904876c4006ab87f
morsssch <morsssovski@gmail.com>
2025-06-26 19:15:39 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/summer-practice-2025/sw.js', { scope: '/summer-practice-2025/' })})}
